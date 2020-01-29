ce76898e230302fcb08836d2fc1db6b8c7e96dc4
Nelson Davis <nelson6400@gmail.com>
2020-01-28 16:33:23 -0800

    Changed the message

--- Greeter.java
+++ Greeter.java
@@ -1,5 +1,5 @@
 public class Greeter {
-    public static void main(String args[]) {
-        System.out.println("I am Nelson");
+    public static void main(String[] args[]) {
+        System.out.println("Hello Jodi!");
     }
 }
